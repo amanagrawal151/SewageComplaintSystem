85ef5a558245b8dadfe4967d48bbe0a47d801ba6
Aman <btech10215.20@bitmesra.ac.in>
2023-04-01 13:05:27 +0530

    connected to online db

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 function connect () {
     mongoose.set('useCreateIndex', true);
-    mongoose.connect('mongodb://localhost/complaintapp',{useNewUrlParser: true})
+    mongoose.connect('mongodb+srv://amanagrawal:Aman@sewage@atlascluster.aww5ops.mongodb.net/sewageComplaint?retryWrites=true&w=majority',{useNewUrlParser: true})
 }
 
 module.exports = connect
